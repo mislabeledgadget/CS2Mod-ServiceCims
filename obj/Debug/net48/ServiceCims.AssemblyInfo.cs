39d84274c55e1dbd1622f9bb0500ad16b92f71ae
Mislabeled Gadget <turquoise.explorer@gmail.com>
2026-01-30 23:08:22 -0500

    updated with discord link and latest deploy

--- obj/Debug/net48/ServiceCims.AssemblyInfo.cs
+++ obj/Debug/net48/ServiceCims.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServiceCims")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e926a683017df3cbcfbd310f7bd4932a51bb6e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServiceCims")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServiceCims")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
